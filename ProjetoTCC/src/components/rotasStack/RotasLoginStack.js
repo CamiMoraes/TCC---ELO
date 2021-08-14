fba9fc65307dc58b850a9146183599fb5b275567
CamiMoraes <camilasoumoraes@gmail.com>
2021-08-14 14:15:54 +0000

    2

--- ProjetoTCC/src/components/rotasStack/RotasLoginStack.js
+++ ProjetoTCC/src/components/rotasStack/RotasLoginStack.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator } from '@react-navigation/stack';
 import Login from '../../views/Login/Login';
 import RotasTab from '../rotasTab/RotasTab';
 import estiloRotasStack from './estiloRotasStack';
